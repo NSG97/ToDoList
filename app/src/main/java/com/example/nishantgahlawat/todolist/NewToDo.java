74b6bc87704935a645652e696e291aa52e5a8744
NSG97 <nishant15151@iiitd.ac.in>
2017-07-06 18:00:26 +0530

    better readability

--- app/src/main/java/com/example/nishantgahlawat/todolist/NewToDo.java
+++ app/src/main/java/com/example/nishantgahlawat/todolist/NewToDo.java
@@ -41,13 +41,13 @@ public class NewToDo extends AppCompatActivity {
 
         DatePickerET.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Calendar calendar = Calendar.getInstance();
                 DatePickerDialog datePickerDialog = new DatePickerDialog(NewToDo.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
-                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         Calendar newDate = Calendar.getInstance();
-                        newDate.set(i,i1,i2);
+                        newDate.set(year,month,dayOfMonth);
                         DatePickerET.setText(new SimpleDateFormat("dd-MM-yyyy").format(newDate.getTime()));
                     }
                 },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
@@ -61,10 +61,10 @@ public class NewToDo extends AppCompatActivity {
                 Calendar calendar = Calendar.getInstance();
                 TimePickerDialog timePickerDialog = new TimePickerDialog(NewToDo.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                    public void onTimeSet(TimePicker timePicker, int hour_of_day, int minute) {
                         Calendar newTime = Calendar.getInstance();
-                        newTime.set(Calendar.HOUR_OF_DAY,i);
-                        newTime.set(Calendar.MINUTE,i1);
+                        newTime.set(Calendar.HOUR_OF_DAY,hour_of_day);
+                        newTime.set(Calendar.MINUTE,minute);
                         TimePickerET.setText(new SimpleDateFormat("kk:mm a").format(newTime.getTime()));
                     }
                 },calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false);
